91d68bb44bd2e68adba94dffaa48d61c3ec828b2
tchame44 <chamot.antoine@gmail.com>
2019-12-01 22:28:52 +0100

    add backup button

--- src/main/java/io/vertx/guides/wiki/http/HttpServerVerticle.java
+++ src/main/java/io/vertx/guides/wiki/http/HttpServerVerticle.java
@@ -17,8 +17,10 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 import io.vertx.guides.wiki.database.WikiDatabaseService;
@@ -205,6 +207,36 @@ public class HttpServerVerticle extends AbstractVerticle {
 					fileObject.put("name", page.getString("NAME"));
 					fileObject.put("content", page.getString("CONTENT"));
 					filesObject.add(fileObject);
+				});
+				
+				webClient.post(443, "snippets.glot.io", "/snippets")
+				.putHeader("Content-Type", "application/json")
+				.as(BodyCodec.jsonObject())
+				.sendJson(payload, ar -> {
+					if(ar.succeeded()) {
+						HttpResponse<JsonObject> response = ar.result();
+						if(response.statusCode() == 200) {
+							String url = "https://glot.io/snippets/" + response.body().getString("id");
+				              context.put("backup_gist_url", url); 
+				              indexHandler(context);
+						}else {
+							StringBuilder message = new StringBuilder()
+									.append("Could no backup the wiki :")
+									.append(response.statusMessage());
+							JsonObject body = response.body();
+							if(body != null) {
+								message.append(System.getProperty("line.separator"))
+								.append(body.encodePrettily());
+							}
+							LOGGER.error(message.toString());
+							context.fail(502);
+						}
+						
+					}else {
+						Throwable err = ar.cause();
+			            LOGGER.error("HTTP Client error", err);
+			            context.fail(err);
+					}
 				});
 			}else {
 				context.fail(reply.cause());
